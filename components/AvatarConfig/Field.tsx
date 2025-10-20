5fe05487d1571cbebc0c1c987da45aca4946afe0
MuhammadAbdullahHabib-Techfooot <abdullah.habib@techfootintl.com>
2025-10-20 23:37:14 +0500

    fix

--- components/AvatarConfig/Field.tsx
+++ components/AvatarConfig/Field.tsx
@@ -3,7 +3,7 @@ interface FieldProps {
   children: React.ReactNode;
   tooltip?: string;
 }
-
+// Hello
 export const Field = (props: FieldProps) => {
   return (
     <div className="flex flex-col gap-1">
